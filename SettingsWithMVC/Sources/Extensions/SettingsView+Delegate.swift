60f8cbe6ff560ade608bb9a35bb3e8740ab96c80
AisauleS <aisaulesibatova@gmail.com>
2023-03-21 12:04:36 +0500

    Updated delegate functions

--- SettingsWithMVC/Sources/Extensions/SettingsView+Delegate.swift
+++ SettingsWithMVC/Sources/Extensions/SettingsView+Delegate.swift
@@ -12,20 +12,7 @@ import UIKit
 extension SettingsView: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let viewController = DetailViewController()
         tableView.deselectRow(at: indexPath, animated: true)
-        let type = models[indexPath.section].options[indexPath.row]
-        viewController.model = models[indexPath.section].options[indexPath.row]
-        
-        switch type {
-        case .regularCell(let model):
-            //            navigationController?.pushViewController(viewController, animated: true)
-            viewController.fillSettings(with: model.title)
-        case .switchCell(_):
-            print("nothing happens")
-        case .statusCell(let model):
-            //            navigationController?.pushViewController(viewController, animated: true)
-            viewController.fillSettings(with: model.title)
         }
     }
-}
+
